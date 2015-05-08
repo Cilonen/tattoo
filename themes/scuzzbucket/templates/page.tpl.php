9f399898896a2ad8d216ad4f6371c71922f80b21
lildestro <niallrmorgan@gmail.com>
2015-05-07 23:40:46 -0700

    Theming changes and adding fonts

--- themes/scuzzbucket/templates/page.tpl.php
+++ themes/scuzzbucket/templates/page.tpl.php
@@ -4,11 +4,6 @@
 
   <header id="header">
     <div class="container">
-      <?php if ($logo): ?>
-        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
-          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
-        </a>
-      <?php endif; ?>
 
       <?php if ($site_name || $site_slogan): ?>
         <div id="name-and-slogan">
@@ -54,8 +49,13 @@
 
   <div id="main">
     <div class="container">
+      <?php if ($logo): ?>
+        <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" id="logo">
+          <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
+        </a>
+      <?php endif; ?>
       <section id="content">
-  
+
           <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
             <div id="content-header">
 
@@ -65,13 +65,6 @@
                 <div id="highlighted"><?php print render($page['highlighted']) ?></div>
               <?php endif; ?>
 
-              <?php print render($title_prefix); ?>
-
-              <?php if ($title): ?>
-                <h1 class="title"><?php print $title; ?></h1>
-              <?php endif; ?>
-
-              <?php print render($title_suffix); ?>
               <?php print $messages; ?>
               <?php print render($page['help']); ?>
 
