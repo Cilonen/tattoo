2c92792692d9cb725bd6c1e51578fbdf66493d3c
lildestro <niallrmorgan@gmail.com>
2015-05-14 00:38:29 -0700

    Theming for the homepage

--- themes/scuzzbucket/templates/page.tpl.php
+++ themes/scuzzbucket/templates/page.tpl.php
@@ -5,28 +5,6 @@
   <header id="header">
     <div class="container">
 
-      <?php if ($site_name || $site_slogan): ?>
-        <div id="name-and-slogan">
-
-          <?php if ($site_name): ?>
-            <?php if ($title): ?>
-              <div id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-              </div>
-            <?php else: /* Use h1 when the content title is empty */ ?>
-              <h1 id="site-name">
-                <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
-              </h1>
-            <?php endif; ?>
-          <?php endif; ?>
-
-          <?php if ($site_slogan): ?>
-            <div id="site-slogan"><?php print $site_slogan; ?></div>
-          <?php endif; ?>
-
-        </div>
-      <?php endif; ?>
-
       <?php if ($page['header']): ?>
         <div id="header-region">
           <?php print render($page['header']); ?>
@@ -54,6 +32,24 @@
           <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>"/>
         </a>
       <?php endif; ?>
+
+      <?php if ($site_name || $site_slogan): ?>
+        <div id="name-and-slogan">
+
+          <?php if ($site_name): ?>
+            <h1 id="site-name">
+              <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home"><?php print $site_name; ?></a>
+            </h1>
+          <?php endif; ?>
+
+          <?php if ($site_slogan): ?>
+            <div id="site-slogan"><?php print $site_slogan; ?></div>
+          <?php endif; ?>
+
+        </div>
+      <?php endif; ?>
+
+
       <section id="content">
 
           <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
